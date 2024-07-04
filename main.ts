64f0f19707a102ef4d9eb99b121a594caf3042d9
MadeDokeMaper <moneyblower@gmx.ch>
2024-07-04 13:36:52 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 basic.forever(function () {
 	//% color=190 weight=100 icon="\uf1ec" block="Motorsteuerung"
 	namespace Motosteuerung {
-		basic.show_number(99)
+		//% blockId=Motorvortrieb
+		//% block="Motor"
+		export function motorkraft():void{
+			let Antrieb_links
+			let Antrieb_rechts
+			const Minimal_Antrieb
+			pins.analogWritePin(AnalogPin.P1; Math.map(Antrieb_links, 0, 50, Minimal_Antrieb, 1023))
+			pins.analogWritePin(AnalogPin.P2; Math.map(Antrieb_rechts, 0, 50, Minimal_Antrieb, 1023))
+		}
 	}
 	
 })
