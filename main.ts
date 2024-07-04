b9b5ad0b88a6c2a27a90f663a57a0056268d3f32
MadeDokeMaper <moneyblower@gmx.ch>
2024-07-04 14:10:29 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,18 @@
-basic.forever(function () {
-	//% color=190 weight=100 icon="\uf1ec" block="Motorsteuerung"
-	namespace Motorsteuerung {
-		//% blockId=Motorvortrieb
-		//% block="Motor"
-		//% linksKraft.min=-10 linksKraft.max=10
-    		//% rechtsKraft.min=-10 rechtsKraft.max=10
-		export function motorkraft(linksKraft:number, rechtsKraft:number):void{
-			let Antrieb_links
-			let Antrieb_rechts
-			const Minimal_Antrieb
-			Antrieb_links=linksKraft
-			Antrieb_rechts=rechtsKraft
-			pins.analogWritePin(AnalogPin.P1; Math.map(Antrieb_links, 0, 50, Minimal_Antrieb, 1023))
-			pins.analogWritePin(AnalogPin.P2; Math.map(Antrieb_rechts, 0, 50, Minimal_Antrieb, 1023))
-		}
+//% color=190 weight=100 icon="\uf1ec" block="Motorsteuerung"
+namespace Motorsteuerung {
+    //% blockId=Motorvortrieb
+    //% block="Motor"
+    //% linksKraft.min=-10 linksKraft.max=10
+    //% rechtsKraft.min=-10 rechtsKraft.max=10
+    export function motorkraft(linksKraft: number, rechtsKraft: number): void {
+        let Antrieb_links
+        let Antrieb_rechts
+        const Minimal_Antrieb
+        Antrieb_links = linksKraft
+        Antrieb_rechts = rechtsKraft
+        pins.analogWritePin(AnalogPin.P1, Math.map(Antrieb_links, 0, 50, Minimal_Antrieb, 1023))
+        pins.analogWritePin(AnalogPin.P2, Math.map(Antrieb_rechts, 0, 50, Minimal_Antrieb, 1023))
+		
 	}
 	
-})
+}
